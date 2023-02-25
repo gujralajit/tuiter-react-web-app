db07a96fa5d2bb17626561a3ba2e4969931f710f
gujralajit <gujralajit@gmail.com>
2023-02-25 11:41:09 -0500

    Added who to follow list item to tuiter.

---
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -0,0 +1,24 @@
+const WhoToFollowListItem = ({
+                                 who = {userName: 'NASA', handle: 'NASA', avatarIcon: '../../img/nasa-icon.png'}
+                             }) => {
+    return (
+    <li class="list-group-item">
+    <div class="row">
+        <div class="col-2">
+            <img class="rounded-circle" height={48} src={`../../img/${who.avatarIcon}`}/>
+        </div>
+        <div class="col-8">
+            <div class="fw-bolder">{who.userName}
+                <img src="../../img/verified-icon.png" height={12} class="rounded-circle"/>
+            </div>
+            <div class="text-secondary">@{who.handle}</div>
+        </div>
+        <div class="col-2">
+            <button type="button" class="btn btn-primary rounded-pill float-end">Follow</button>
+        </div>
+    </div>
+    </li>
+   );
+};
+
+export default WhoToFollowListItem;
