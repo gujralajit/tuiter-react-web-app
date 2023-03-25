d5ea0f53e9b8c6a6656728d336aaa8df408f380a
gujralajit <gujralajit@gmail.com>
2023-03-25 16:39:12 -0400

    Refactored post summary list component to use redux.

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -5,9 +5,10 @@ import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
 import HomeComponent from "./home";
 import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
 import { configureStore } from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
-const store = configureStore({reducer: {who: whoReducer}});
+const store = configureStore({reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
     return (
